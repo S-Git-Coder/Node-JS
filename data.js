16f89905cbdc6b33495a7d2fc3c50b88a9a2c4ba
S-Git-Coder <sahilpatel9313@gmail.com>
2024-09-17 00:37:09 +0530

    This is a data file

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+    {name:'Sahil patel', email:'sahil@gmail.com'},
+    {name:'Kevin patel', email:'kevin@gmail.com'},
+    {name:'patel', email:'patel@gmail.com',contact:'991100'}
+
+]
+    module.exports = data; 
